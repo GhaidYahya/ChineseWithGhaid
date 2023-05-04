ffd993c9d06f82419d97ca535fe3f37487028309
ghaid <ghaidyahya@gmail.com>
2023-05-04 13:42:07 +0300

    Chinese with Ghaid App to Test your Chinese

---
+++ app/src/main/java/com/example/learnchinese/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.learnchinese;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button1;
+    Button button2;
+    TextView questionsTxtV;
+    TextView totalQuestionsN;
+    Button Submit_button;
+    int score =0;
+    int totalQuestions=QuestionsANDanswers.Question.length;
+    int CurrentQuestionIndex=0;
+    String SelectedAnswer="";
+    int my_color ;
+    int my_yel ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         my_color = ContextCompat.getColor(this, R.color.pink2);
+          my_yel = ContextCompat.getColor(this, R.color.yellow);
+        questionsTxtV = findViewById(R.id.questionsTxtV);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        Submit_button = findViewById(R.id.Submit_button);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        Submit_button.setOnClickListener(this);
+        totalQuestionsN = findViewById(R.id.totalQuestionsN);
+        totalQuestionsN.setText(String.valueOf(totalQuestions));
+        loadNewQuestion();
+    }
+    @Override
+    public void onClick(View v) {
+
+        button1.setBackgroundColor(my_color);
+        button2.setBackgroundColor(my_color);
+        Button clickedButton = (Button) v;
+
+        if(clickedButton.getId()==R.id.Submit_button){
+            if(SelectedAnswer.equals(QuestionsANDanswers.CorrAnswers[CurrentQuestionIndex])){
+                score++;
+            }
+            CurrentQuestionIndex++;
+            loadNewQuestion();
+
+
+
+        }else {
+            //other choices
+            SelectedAnswer = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(my_yel);
+        }
+
+    }
+    void loadNewQuestion(){
+        if(CurrentQuestionIndex == totalQuestions){
+            ENDQuiz();
+            return;
+        }
+        questionsTxtV.setText(QuestionsANDanswers.Question[CurrentQuestionIndex]);
+        button1.setText(QuestionsANDanswers.Choices[CurrentQuestionIndex][0]);
+        button2.setText(QuestionsANDanswers.Choices[CurrentQuestionIndex][1]);
+    }
+    void ENDQuiz(){
+        String passG="";
+        if(score > 0.60*totalQuestions){
+            passG="PASSED :)";
+        }
+        else{
+            passG="FAILED :(";
+        }
+
+        new AlertDialog.Builder(this).setTitle(passG).setMessage("Your Score is "+score +"/" + totalQuestions).setPositiveButton("RESTART",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+
+    }
+    void restartQuiz(){
+        score = 0;
+        CurrentQuestionIndex=0;
+        loadNewQuestion();
+    }
+}
---
+++ app/src/main/java/com/example/learnchinese/QuestionsANDanswers.java
@@ -0,0 +1,30 @@
+package com.example.learnchinese;
+
+public  class QuestionsANDanswers {
+
+    public static String Question[] = {
+            "the word CAT in Chinese is :",
+            "the word HUMAN in Chinese is :",
+            "the word TEACHER in Chinese is :",
+            "the pronoun YOU in Chinese is :",
+            "the word HELLO in Chinese is :",
+
+};
+
+    public static String Choices[][] = {
+            {"Gǒu","Māo"},
+            {"Rén","Hāo"},
+            {"Xuéshēng","Lǎoshī"},
+            {"Nǐ","Wǒ"},
+            {"Nǐ hǎo","Zàijiàn"}
+
+    };
+
+    public static String CorrAnswers[] = {
+            "Māo", "Rén","Lǎoshī","Nǐ","Nǐ hǎo"
+
+    };
+
+
+
+}
